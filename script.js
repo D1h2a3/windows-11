064f6eae2d86e0c35808b1fe0b91a11adf05e45f
D1h2a3 <86161176+D1h2a3@users.noreply.github.com>
2021-11-15 18:52:21 +0530

    File
    Optional

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("our website is only for laptop and desktop ,but you can use on phone by turning desktop mode in your browser")
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
